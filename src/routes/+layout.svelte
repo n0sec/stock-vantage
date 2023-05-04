7657f96c6a6f9cde8ec3671687ad8eb11cbc0398
Nick <nconkl1@outlook.com>
2023-05-04 18:16:12 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,141 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { AppBar, AppRail, AppRailTile, AppShell, LightSwitch } from '@skeletonlabs/skeleton';
+	import { writable, type Writable } from 'svelte/store';
+
+	const storeValue: Writable<number | null> = writable(null);
+	export const loggedIn: boolean = false;
+</script>
+
+<svelte:head>
+	<title>Stock Vantage</title>
+</svelte:head>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- AppBar with Light Switch-->
+		<AppBar class="w-full">
+			<svelte:fragment slot="lead">
+				<!-- Logo -->
+				<a href="/" class=""><h1 class="unstyled text-xl font-bold">Stock Vantage</h1></a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<div class="font-bold">
+					{#if loggedIn}
+						<a href="/logout" class="mr-6">Logout</a>
+					{:else}
+						<a href="/signup" class="mx-6">Sign Up</a>
+						<a href="/login" class="mr-6">Login</a>
+					{/if}
+				</div>
+				<input type="search" placeholder="Search..." class="rounded-xl variant-ghost-surface" />
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail selected={storeValue}>
+			<!-- Left Sidebar Content -->
+			<!-- * Dashboard * -->
+			{#if loggedIn}
+				<AppRailTile label="Dashboard" value={0} tag="a" href="/dashboard"
+					><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"
+						/></svg
+					></AppRailTile
+				>
+				<!-- * Watchlist * -->
+				<AppRailTile label="Watchlist" value={1} tag="a" href="/watchlist"
+					><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5C22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1l-.1-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05z"
+						/></svg
+					></AppRailTile
+				>
+			{/if}
+			<!-- * Crypto * -->
+			<AppRailTile label="Crypto" value={2} tag="a" href="/crypto"
+				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M17.06 11.57c.59-.69.94-1.59.94-2.57c0-1.86-1.27-3.43-3-3.87V3h-2v2h-2V3H9v2H6v2h2v10H6v2h3v2h2v-2h2v2h2v-2c2.21 0 4-1.79 4-4c0-1.45-.78-2.73-1.94-3.43zM10 7h4c1.1 0 2 .9 2 2s-.9 2-2 2h-4V7zm5 10h-5v-4h5c1.1 0 2 .9 2 2s-.9 2-2 2z"
+					/></svg
+				></AppRailTile
+			>
+			<!-- * Forex * -->
+			<AppRailTile label="Forex" value={3} tag="a" href="/forex"
+				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M12.89 11.1c-1.78-.59-2.64-.96-2.64-1.9c0-1.02 1.11-1.39 1.81-1.39c1.31 0 1.79.99 1.9 1.34l1.58-.67c-.15-.45-.82-1.92-2.54-2.24V5h-2v1.26c-2.48.56-2.49 2.86-2.49 2.96c0 2.27 2.25 2.91 3.35 3.31c1.58.56 2.28 1.07 2.28 2.03c0 1.13-1.05 1.61-1.98 1.61c-1.82 0-2.34-1.87-2.4-2.09l-1.66.67c.63 2.19 2.28 2.78 2.9 2.96V19h2v-1.24c.4-.09 2.9-.59 2.9-3.22c0-1.39-.61-2.61-3.01-3.44zM3 21H1v-6h6v2H4.52c1.61 2.41 4.36 4 7.48 4a9 9 0 0 0 9-9h2c0 6.08-4.92 11-11 11c-3.72 0-7.01-1.85-9-4.67V21zm-2-9C1 5.92 5.92 1 12 1c3.72 0 7.01 1.85 9 4.67V3h2v6h-6V7h2.48C17.87 4.59 15.12 3 12 3a9 9 0 0 0-9 9H1z"
+					/></svg
+				></AppRailTile
+			>
+
+			<!-- * Gainers *-->
+			<AppRailTile label="Gainers" value={4} tag="a" href="/gainers"
+				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="m16 6l2.29 2.29l-4.88 4.88l-4-4L2 16.59L3.41 18l6-6l4 4l6.3-6.29L22 12V6z"
+					/></svg
+				></AppRailTile
+			>
+
+			<AppRailTile label="Losers" value={5} tag="a" href="/losers"
+				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="m16 18l2.29-2.29l-4.88-4.88l-4 4L2 7.41L3.41 6l6 6l4-4l6.3 6.29L22 12v6z"
+					/></svg
+				></AppRailTile
+			>
+
+			<!-- * News * -->
+			<AppRailTile label="News" value={6} tag="a" href="/news"
+				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="m22 3l-1.67 1.67L18.67 3L17 4.67L15.33 3l-1.66 1.67L12 3l-1.67 1.67L8.67 3L7 4.67L5.33 3L3.67 4.67L2 3v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V3zM11 19H4v-6h7v6zm9 0h-7v-2h7v2zm0-4h-7v-2h7v2zm0-4H4V8h16v3z"
+					/></svg
+				></AppRailTile
+			>
+
+			<!-- Profile icon -->
+			<!-- ! Only show if logged in ! -->
+			<svelte:fragment slot="trail">
+				{#if loggedIn}
+					<AppRailTile label="Profile" value={7} tag="a" href="/profile">
+						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+							><path
+								fill="currentColor"
+								d="M12 19.2c-2.5 0-4.71-1.28-6-3.2c.03-2 4-3.1 6-3.1s5.97 1.1 6 3.1a7.232 7.232 0 0 1-6 3.2M12 5a3 3 0 0 1 3 3a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3m0-3A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10c0-5.53-4.5-10-10-10Z"
+							/></svg
+						>
+					</AppRailTile>
+				{/if}
+			</svelte:fragment>
+		</AppRail>
+	</svelte:fragment>
+
+	<!-- A floating action button in the bottom right -->
+	<!-- ? Might not need this ? -->
+	<button type="button" class="btn-icon btn-icon-xl variant-filled absolute bottom-6 right-6"
+		><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+			><path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2Z" /></svg
+		></button
+	>
+
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+</AppShell>
